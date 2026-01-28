694f6b369441748668b95f7e2d8b818a8ff3b64f
Yevhenii Yermolenko - UIXER <evgeniu.ermolenko@gmail.com>
2026-01-28 06:45:53 +0100

    Stable version

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link'
+
+const links = [
+	{ href: '/', label: 'Home' },
+	{ href: '/ui', label: 'UI' },
+	{ href: '/auth/register', label: 'Registration' },
+	{ href: '/Login', label: 'Login' },
+]
+
 export default function Page() {
-	return <div>!</div>;
+	return <div>{
+		<ol>
+			{links.map(({ href, label }) => <li key={href}><Link href={`pl/${href}`}>{label}</Link></li>)}
+		</ol>
+	}</div>;
 }
