6d90d2796ec838643039aa8ddfcce6e42e03d679
Yevhenii Yermolenko <evgeniu.ermolenko@gmail.com>
2026-06-18 20:31:46 +0200

    Changed version

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,6 +1,6 @@
 export const APP_CONFIG = {
 	name: "StworzEvent.pl",
-	version: "0.0.1",
+	version: "0.0.4",
 	description: "Platforma do tworzenia i zarządzania wydarzeniami",
 	url: "https://stworzevent.pl",
 	email: {
